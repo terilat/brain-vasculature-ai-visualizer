33d893f0da66be307907f18a4aa6dc40c91a1806
terilat <terilat34634@yandex.ru>
2025-08-24 16:18:11 +0300

    cursor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/brain-vasculature-ai-visualizer">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/gallery" element={<Gallery />} />
